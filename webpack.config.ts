406353a23340898c597af13d32fefe8157baccb3
Koranin Suwannaprasert <koranin@descarteslabs.com>
2019-07-31 11:14:48 -0600

    Clean up webpack config.

--- webpack.config.ts
+++ webpack.config.ts
@@ -3,28 +3,29 @@ import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 const config: webpack.Configuration = {
+  mode: 'development',
+  devtool: 'source-map',
   entry: path.join(__dirname, '/src/index.tsx'),
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js',
+  },
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
-        loader: 'babel-loader',
-      },
-      {
-        enforce: 'pre',
-        test: /\.js$/,
-        loader: 'source-map-loader',
+        test: /\.(ts|tsx)?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
       },
     ],
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js'],
   },
-  output: {
-    path: path.join(__dirname, '/dist'),
-    filename: 'bundle.js',
-  },
-  plugins: [new HtmlWebpackPlugin({ template: 'index.html' })],
+  plugins: [
+    new HtmlWebpackPlugin({ template: 'index.html' }),
+    new webpack.ProgressPlugin(),
+  ],
 };
 
 export default config;
