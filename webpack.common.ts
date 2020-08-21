bc183fdda70d83854ab180d33e54cb67580680eb
koranins <koranin@descarteslabs.com>
2020-08-20 22:15:32 -0600

    Update webpac

--- webpack.common.ts
+++ webpack.common.ts
@@ -1,15 +1,7 @@
-import webpack, { Configuration, Loader } from 'webpack';
+import webpack, { Configuration } from 'webpack';
 import path from 'path';
 import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-
-const typescriptReactLoader: Loader = {
-  loader: 'babel-loader',
-  options: {
-    presets: ['@babel/react', '@babel/typescript'],
-    plugins: [['styled-components', { ssr: false }]],
-  },
-};
 
 const config: Configuration = {
   entry: './src/index.tsx',
@@ -22,7 +14,13 @@ const config: Configuration = {
       {
         test: /\.(ts|tsx)?$/,
         exclude: /node_modules/,
-        use: typescriptReactLoader,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/react', '@babel/typescript'],
+            plugins: [['styled-components', { ssr: false }]],
+          },
+        },
       },
     ],
   },
@@ -32,7 +30,7 @@ const config: Configuration = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({ template: 'app-template.html' }),
-    new webpack.ProgressPlugin(),
+  new webpack.ProgressPlugin({}),
   ],
 };
 
